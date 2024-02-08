c63aaab54cd6ec01a12612dffb35530c1858433a
suhailak22 <139833962+suhailak22@users.noreply.github.com>
2024-02-08 12:07:42 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /app/github_suhailak22_cloudbuild-to-gce
 git fetch
-git pull main
+git pull
 cp -r * /var/www/html
 rm -rf /var/www/html/.git
 chown -R www-data:www-data /var/www/html/*
